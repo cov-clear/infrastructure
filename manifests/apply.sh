5d27e284c3f34c416584cd57622e27205e3a7667
Jose Galarza <igalarzab@gmail.com>
2020-04-27 11:39:14 +0100

    Create kube-ingress namespace

--- manifests/apply.sh
+++ manifests/apply.sh
@@ -8,7 +8,7 @@ fi
 
 # Delete super permissive PodSecurityPolicy
 kubectl delete psp/eks.privileged
-kubectl delete clusterrole/eks:podsecuritypolicy:privilegedkcs
+kubectl delete clusterrole/eks:podsecuritypolicy:privileged
 kubectl delete clusterrolebinding/eks:podsecuritypolicy:authenticated
 
 # Add our own security policy
@@ -18,6 +18,7 @@ kubectl apply -f security/pod-security-policies.yaml
 kubectl patch -n kube-system deployment/coredns --type json -p='[{"op": "remove", "path": "/spec/template/metadata/annotations/eks.amazonaws.com~1compute-type"}]'
 
 # Install the ALB ingress controller
+kubectl create namespace kube-ingress
 kubectl apply -f alb-ingress-controller/rbac.yaml
 sed "s/\[\[vpc_id\]\]/$1/g;s/\[\[vpc_region\]\]/$2/g;s/\[\[cluster_name\]\]/$3/g" alb-ingress-controller/deployment.yaml | kubectl apply -f -
 kubectl annotate --overwrite serviceaccount -n kube-ingress default eks.amazonaws.com/role-arn=$4
